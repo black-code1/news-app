dedadc2cb25ce9a512b5d1e11b825a3bee1667a3
black-code1 <legrandmahamat1@gmail.com>
2021-06-06 09:15:25 +0100

    remove unecessary margins on each bloc of the image

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,15 @@
 const News = ({urlToImage,source, title, description, publishedAt}) => (
 
   <div>
-    <div className="px-8 py-12 max-w-sm rounded-lg bg-white">
+    <div className="px-6 py-8 max-w-sm rounded-xl shadow-xl bg-white">
       <img 
       src={urlToImage}  
       alt={title}
-      className="-mt-16 rounded-lg shadow-xl" />
-      <button className="bg-red-500 text-white text-sm mt-6 ml-2 py-1 px-2 rounded">{source.name}</button>
-      <h1 className="ml-2 mt-1 font-bold">{title}</h1>
-      <p className="ml-2 mt-2 text-sm text-gray-500">{description}</p>
-      <div className="ml-2 mt-1 flex items-center">
+      className="-mt-16 rounded-xl shadow-xl" />
+      <button className="bg-red-500 text-white text-xs mt-6 ml-2 py-1 px-2 rounded-2xl">{source.name}</button>
+      <h1 className="mt-1 font-bold">{title}</h1>
+      <p className="mt-2 text-sm text-gray-500">{description}</p>
+      <div className="mt-1 flex items-center">
       <img 
       src={urlToImage}  
       alt={title}
