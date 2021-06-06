8740a48d04af5042dd6b19e60f398a96b46e304a
black-code1 <legrandmahamat1@gmail.com>
2021-06-06 09:01:08 +0100

    redesigned the image style by adding a negative margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ const Footer = () => (
       <div className="p-2">
         <h3 className="font-bold mb-1 text-center">Subscribe to our NewsLetter</h3>
         <p className="text-sm text-center mb-2">Sign up free and be the first to get notified about new posts.</p>
-        <div className="text-center">
+        <div className="text-center flex">
            <input type="text" placeholder="Your email adress" className="w-full border border-gray-300 px-3 py-2 rounded-full shadow-sm focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-200" />
-        <button>Subscribe</button> 
+        <button className="bg-red-500">Subscribe</button> 
         </div>
       
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,11 @@
 const News = ({urlToImage,source, title, description, publishedAt}) => (
 
   <div>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg h-full bg-white">
+    <div className="px-8 py-12 max-w-sm rounded-lg bg-white">
       <img 
       src={urlToImage}  
       alt={title}
-      className="w-full" />
+      className="-mt-16 rounded-lg shadow-xl" />
       <button className="bg-red-500 text-white text-sm mt-6 ml-2 py-1 px-2 rounded">{source.name}</button>
       <h1 className="ml-2 mt-1 font-bold">{title}</h1>
       <p className="ml-2 mt-2 text-sm text-gray-500">{description}</p>
